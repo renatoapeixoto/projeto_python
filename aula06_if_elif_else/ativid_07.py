13bc9940ebe723de5e9b1332cdacb161940ee90b
renatoapeixoto <peixoto.sd@gmail.com>
2024-10-30 13:56:26 -0300

    refinamento

--- aula06_if_elif_else/ativid_07.py
+++ aula06_if_elif_else/ativid_07.py
@@ -1,3 +1,7 @@
+# G) Você está desenvolvendo um programa para determinar se três segmentos podem
+# formar um triângulo. Para isso, o programa precisa receber as medidas dos três
+# segmentos e compará-las entre si. A resposta resultante dessa comparação deve 
+# indicar se os segmentos fornecidos podem formar um triângulo ou não.
 
 #imports
 import os
@@ -7,6 +11,18 @@ import os
 os.system('cls')
 
 # Título
-print('Verificar se é um triangulo')  
+print('Verificar se os segmentos formam um triangulo')  
 
-# Entradas e Declarações 
+# Entradas e Declarações
+a = float(input('Digite o segmento < a > do triângulo: '))
+b = float(input('Digite o segmento < b > do triângulo: '))
+c = float(input('Digite o segmento < c > do triângulo: '))
+
+# Processamento
+if (a+b)>c and (a+c)>b and (b+c)>a :
+    resultado = 'É um triangulo'
+else :
+    resultado = 'Não é um triangulo'
+
+# Saidas
+print(resultado)
--- aula06_if_elif_else/ativid_08.py
+++ aula06_if_elif_else/ativid_08.py
@@ -1,3 +1,10 @@
+# H) A empresa "RootCalc" está desenvolvendo um software de cálculo de raízes de 
+# equações quadráticas para auxiliar engenheiros e cientistas em suas análises e 
+# projetos. Eles precisam de um programa que calcule as raízes da equação 
+# quadrática 𝑥²−6𝑥+5 sem depender de funções ou métodos predefinidos, utilizando 
+# apenas os conceitos e operações básicas aprendidos até o momento. Essas raízes
+# são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser capaz de calcular 
+# essas raízes de forma precisa, seguindo os princípios matemáticos fundamentais.
 
 #imports
 import os
@@ -7,7 +14,9 @@ import os
 os.system('cls')
 
 # Título
-print('Equação do segundo grau ax² + bx + c = 0')  
+print("-"*70)
+print('Equação do segundo grau a.x² + b.x + c = 0')  
+print("-"*70)
 
 # Entradas e Declarações
 a = int(input('Digite o valor de a: '))
