d36c10b88f8b06176bdc41e1c2cf03beeb6bc9ea
renatoapeixoto <peixoto.sd@gmail.com>
2024-11-12 15:17:01 -0300

    up

--- Estudo/Chatgpt/a.py
+++ Estudo/Chatgpt/a.py
@@ -5,7 +5,7 @@ fibonacci = [0, 1]
 while fibonacci[-1] <= B:
     fibonacci.append(fibonacci[-1] + fibonacci[-2])
 
-print("Números de Fibonacci no intervalo [", A, ",", B, "]:")
+print("Números de Fibonacci no intervalo: [", A, ",", B, "]:")
 for num in fibonacci:
     if A <= num <= B:
         print(num, end=" ")
